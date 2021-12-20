914f382e7eab86b174e926beeffce1dfa4388c3d
José Victor D. Fernandes <josevictordf@gmail.com>
2021-12-20 16:46:27 -0300

    Section 5
    Use SwiftUI Views From Other Frameworks

---
+++ Landmarks/MapView.swift.swift
@@ -0,0 +1,25 @@
+//
+//  MapView.swift.swift
+//  Landmarks
+//
+//  Created by Jose Victor Fernandes on 20/12/21.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView_swift: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct MapView_swift_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView_swift()
+    }
+}
